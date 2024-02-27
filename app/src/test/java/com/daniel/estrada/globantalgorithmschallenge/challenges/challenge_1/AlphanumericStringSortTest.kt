c03f5c026270b90017ad8e1adb4fdda194ca3838
Daniel Estrada <isc.daniel.estrada@gmail.com>
2024-02-27 14:49:31 -0600

    Added some unit tests

--- app/src/test/java/com/daniel/estrada/globantalgorithmschallenge/challenges/challenge_1/AlphanumericStringSortTest.kt
+++ app/src/test/java/com/daniel/estrada/globantalgorithmschallenge/challenges/challenge_1/AlphanumericStringSortTest.kt
@@ -14,9 +14,25 @@ class AlphanumericStringSortTest {
     }
 
     @Test
-    fun `First test`() {
+    fun `Basic test`() {
         val input = "®45DCeba"
         val expectedResult = "abeCD54®"
+        val result = alphanumericStringSort.sort(input)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Sort a camel case word, lower case letters must go first`() {
+        val input = "camelCase"
+        val expectedResult = "aaceelmsC"
+        val result = alphanumericStringSort.sort(input)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `Sort string with upper and lower case letters, numbers and special characters`() {
+        val input = "#4321BasiC"
+        val expectedResult = "aisBC1324#"
         val result = alphanumericStringSort.sort(input)
         assertEquals(expectedResult, result)
     }
